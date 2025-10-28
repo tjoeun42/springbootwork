40eea7b2227c8406ac5e12c256198453c3371005
tjoeun42 <tjoeunit@gmail.com>
2025-10-28 15:37:54 +0900

    2일차_1교시_Ex04

---
+++ Ex04_jspSetting/src/main/java/com/study/springboot/controller/MyController.java
@@ -0,0 +1,18 @@
+package com.study.springboot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MyController {
+	@RequestMapping("/")
+	public @ResponseBody String root() {
+		return "jsp로 실행";
+	}
+	
+	@RequestMapping("/test1")
+	public String test1() {
+		return "test1";   //실제 호출될 때  /WEB-INF/views/test1.jsp 호출됨
+	}
+}
