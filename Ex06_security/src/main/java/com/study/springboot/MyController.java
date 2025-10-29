bbecbe2cddcb3fdd7d4af464c54e6722f7ca9ed0
tjoeun42 <tjoeunit@gmail.com>
2025-10-29 17:49:47 +0900

    3일차_3교시 Ex06

---
+++ Ex06_security/src/main/java/com/study/springboot/MyController.java
@@ -0,0 +1,28 @@
+package com.study.springboot;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MyController {
+	@RequestMapping("/")
+	public @ResponseBody String root() {
+		return "Security Home";
+	}
+	
+	@RequestMapping("/guest/welcome")
+	public String welcome1() {
+		return "guest/welcome1";
+	}
+	
+	@RequestMapping("/member/welcome")
+	public String welcome2() {
+		return "member/welcome2";
+	}
+	
+	@RequestMapping("/admin/welcome")
+	public String welcome3() {
+		return "admin/welcome3";
+	}
+}
