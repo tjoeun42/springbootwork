11621ab4b7b7a2e2d50b512a9534b9cf8bf27640
tjoeun42 <tjoeunit@gmail.com>
2025-11-04 19:21:47 +0900

    7일차_4교시_Ex15

--- Ex15_Homepage/src/main/java/com/study/springboot/controller/BoardController.java
+++ Ex15_Homepage/src/main/java/com/study/springboot/controller/BoardController.java
@@ -1,11 +1,17 @@
 package com.study.springboot.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
+import com.study.springboot.domain.Board;
+import com.study.springboot.domain.Member;
 import com.study.springboot.service.BoardService;
 
 @Controller
@@ -15,9 +21,23 @@ public class BoardController {
 	@Autowired
 	BoardService bService;
 	
+	@GetMapping("list")
+	public String list(Model model) {
+		List<Board> list = bService.list();
+		model.addAttribute("list", list);
+		return "board/list";
+	}
+	@PostMapping("/insert")
+	public String insert(Board board) {
+		bService.insert(board);
+		return "redirect:list";
+	}
+	
 	@GetMapping("/insertForm")
-	public String insertForm(Model model) {
+	public String insertForm(@SessionAttribute(value="loginUser", required=false) Member loginUser , Model model) {
 		model.addAttribute("loginUser", loginUser);
 		return "board/insertForm";
 	}
+	
+	
 }
--- Ex15_Homepage/src/main/java/com/study/springboot/service/BoardService.java
+++ Ex15_Homepage/src/main/java/com/study/springboot/service/BoardService.java
@@ -1,8 +1,11 @@
 package com.study.springboot.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.study.springboot.domain.Board;
 import com.study.springboot.repository.BoardRepository;
 
 
@@ -11,5 +14,15 @@ public class BoardService {
 	
 	@Autowired
 	BoardRepository bRepository;
+	
+	public List<Board> list() {
+		return bRepository.findAll();
+	}
+
+	public Board insert(Board board) {
+		return bRepository.save(board);		
+	}
+
+	
 
 }
